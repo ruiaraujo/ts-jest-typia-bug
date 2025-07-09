d2f62652e6fbeeeb79234c34017c109b14d1cdb5
Rui Araujo <rui.araujo@commercetools.com>
2025-07-09 23:42:52 +0100

    Add logs from the two runs

--- jest.config.js
+++ jest.config.js
@@ -4,26 +4,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      compiler: 'typescript',
-      tsconfig: {
-        target: 'es2023',
-        module: 'commonjs',
-        plugins: [
-          {
-            transform: 'typia/lib/transform'
-          }
-        ],
-        strict: true,
-        isolatedModules: true // test pass if this flag is false
-      }
-    }]
-  },
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.ts'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html']
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+  }
 }; 
